61c7fb3c6272f2056ed4c63eb8b8ae1fb491aa32
Stalin Kay <stalin@stalinkay.com>
2021-03-25 22:00:09 +0200

    Remove info.plist generation
    Remove info.plist generation to avoid multiple info.plist error

--- lib/fastlane/plugin/cordova_screenshots/actions/retrofit_cordova_screenshots_ios_action.rb
+++ lib/fastlane/plugin/cordova_screenshots/actions/retrofit_cordova_screenshots_ios_action.rb
@@ -162,10 +162,10 @@ module Fastlane
         #
         # Add files (fastlane configured UI Unit Tests) into target (via test group)
         #
-        UI.message("Adding Pre-Configured UI Unit Tests (*.plist and *.swift) to Test Group '#{scheme_name}'...")
+        UI.message("Adding Pre-Configured UI Unit Tests (*.swift) to Test Group '#{scheme_name}'...")
 
-        files = []
-        Dir["#{config_folder}*.plist", "#{config_folder}*.swift"].each do |file| # config_folder ends with / already
+         files = []
+         Dir["#{config_folder}*.swift"].each do |file| # config_folder ends with / already
           UI.message("Adding UI Test Source '#{file}'")
           files << test_group.new_reference(File.absolute_path(file), '<absolute>')
         end
